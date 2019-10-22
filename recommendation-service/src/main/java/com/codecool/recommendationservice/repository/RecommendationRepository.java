3c6e5cc8a6f9f742168c34ab886c81d688a6c984
GaborPap <ppgabor@gmail.com>
2019-10-22 22:46:06 +0200

    Implemented repository for recommendations.

---
+++ recommendation-service/src/main/java/com/codecool/recommendationservice/repository/RecommendationRepository.java
@@ -0,0 +1,14 @@
+package com.codecool.recommendationservice.repository;
+
+import com.codecool.recommendationservice.model.Recommendation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface RecommendationRepository extends JpaRepository<Recommendation, Long> {
+
+    Optional<Recommendation> findById(long id);
+    Optional<List<Recommendation>> findAllByVideoId(long videoId);
+
+}
