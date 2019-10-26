bd87152cba069bf054cb6743931b35ef936eaf19
GaborPap <ppgabor@gmail.com>
2019-10-27 00:46:38 +0200

    Added cors configuration.

--- api-gateway/src/main/java/com/codecool/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/codecool/apigateway/ApiGatewayApplication.java
@@ -8,6 +8,10 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
+import org.springframework.web.cors.CorsConfiguration;
+
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import springfox.documentation.swagger.web.SwaggerResource;
 import springfox.documentation.swagger.web.SwaggerResourcesProvider;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -45,5 +49,23 @@ public class ApiGatewayApplication {
 		swaggerResource.setLocation("/" + location + "/v2/api-docs");
 		swaggerResource.setSwaggerVersion(version);
 		return swaggerResource;
+	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		final CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("OPTIONS");
+		config.addAllowedMethod("HEAD");
+		config.addAllowedMethod("GET");
+		config.addAllowedMethod("PUT");
+		config.addAllowedMethod("POST");
+		config.addAllowedMethod("DELETE");
+		config.addAllowedMethod("PATCH");
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
 	}
 }
